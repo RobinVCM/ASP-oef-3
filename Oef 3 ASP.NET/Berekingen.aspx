1f78d9393e835fd8fa391e1f7bad42a753315b93
RobinVCM <robin.malfait@viso-cor-mariae.be>
2022-05-16 08:50:20 +0200

    Add project files.

---
+++ Oef 3 ASP.NET/Berekingen.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Berekingen.aspx.cs" Inherits="Oef_3_ASP.NET.Berekingen" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        Getal 1:<asp:TextBox ID="TextBox2" runat="server" Style="margin-left: 11px"></asp:TextBox>
+        &nbsp;<p>
+            &nbsp;Getal 2:&nbsp;
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        </p>
+        <asp:Button ID="btnBereken" runat="server" Text="BEREKEN" OnClick="btnBereken" />
+        <asp:Panel ID="pnlUitkomsten" runat="server" Height="165px">
+            Som:
+            <asp:Label ID="lblSom" runat="server" Text="Label"></asp:Label>
+            <br />
+            Verschil:
+            <asp:Label ID="lblVerschil" runat="server" Text="Label"></asp:Label>
+            <br />
+            Product:<asp:Label ID="lblProduct" runat="server" Text="Label"></asp:Label>
+            <br />
+            Quotient:
+            <asp:Label ID="lblQuotient" runat="server" Text="Label"></asp:Label>
+        </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ Oef 3 ASP.NET/Berekingen.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Oef_3_ASP.NET
+{
+    public partial class Berekingen : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+            {
+                pnlUitkomsten.Visible = true;
+            }
+            else
+            {
+                pnlUitkomsten.Visible = false;
+            }
+        }
+
+
+        protected void btnBereken(object sender, EventArgs e)
+        {
+            double dblGetal1, dblGetal2, dblSom, dblVerschil, dblQuotient, dblProduct;
+
+            dblGetal1 = Convert.ToDouble(TextBox2.Text);
+            dblGetal2 = Convert.ToDouble(TextBox3.Text);
+
+            dblSom = dblGetal1 + dblGetal2;
+            dblProduct = dblGetal1 / dblGetal2;
+            dblVerschil = dblGetal1 - dblGetal2;
+            dblQuotient = dblGetal1 * dblGetal2;
+
+            lblSom.Text = Math.Round(dblSom, 2).ToString();
+            lblVerschil.Text = Math.Round(dblVerschil, 2).ToString();
+            lblProduct.Text = Math.Round(dblProduct, 2).ToString();
+            lblQuotient.Text = Math.Round(dblQuotient, 2).ToString();
+
+        }
+    }
+}
